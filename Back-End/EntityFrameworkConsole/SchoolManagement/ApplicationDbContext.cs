fc6c8614ac5a1b3a9f4d3b120fd6298adcd84d9f
saiful.islam888911 <saiful.islam888911@gmail.com>
2025-10-22 03:55:40 +0600

    Commit No.1 - ET Migration, Single Table, (Hard Coded Input) - commit 1
    # Summary (Short) : Insert, Update, Delete, GetData.
    
    # Project/Code : Summary / Details / Reports :
    ______________________________________________
    
    	i. is incomplete.

---
+++ Back-End/EntityFrameworkConsole/SchoolManagement/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SchoolManagement
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public ApplicationDbContext()
+        {
+            _connectionString = "Server=ASUS-TUF\\SQLEXPRESS;Database=SchoolManagement;" +
+    "User Id=Practice_Admin;Password=12345678;Trust Server Certificate=True;";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Back-End/EntityFrameworkConsole/SchoolManagement/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using SchoolManagement;
+using Microsoft.EntityFrameworkCore;
+
+
+Console.WriteLine("Hello, School Management!");
+
+
+
+
+
+/* ---------------------------
+Topic : Single Table : Student
+------------------------------ */
+
+/* ---------- Insert ---------- */
+
+//___Type : 1___
+/*
+Student student = new Student();
+student.Name = "AAA";
+student.Cgpa = 3.5;
+student.DateOfBirth = new DateTime(2000, 12, 30);
+
+ApplicationDbContext context = new ApplicationDbContext();
+context.Students.Add(student);
+context.SaveChanges();
+*/
+
+//___Type : 2___
+/*
+ApplicationDbContext context = new ApplicationDbContext();
+
+context.Students.Add(new Student()
+{
+    Name = "Mr DDD",
+    Cgpa = 2.5,
+    DateOfBirth = new DateTime(2000, 12, 30)
+});
+context.Students.Add(new Student
+{
+    Name = "Mr EEE",
+    Cgpa = 4,
+    DateOfBirth = new DateTime(2000, 12, 31)
+});
+context.Students.Add(new Student
+{ Name = "Mr FFF", Cgpa = 4, DateOfBirth = new DateTime(2000, 12, 30) });
+context.Students.Add(new Student() { Name = "Mr BBB", Cgpa = 3.5, DateOfBirth = new DateTime(2000, 12, 30) });
+context.Students.Add(new Student { Name = "Mr CCC", Cgpa = 2.5, DateOfBirth = new DateTime(2000, 12, 31) });
+context.SaveChanges();
+*/
+
+
+/* ---------- Get ---------- */
+
+// ApplicationDbContext context = new ApplicationDbContext();
+// Student student = context.Students.Where(x => x.Cgpa <= 3.2).First();
+// Student? student = context.Students.Where(x => x.Cgpa <= 3.2).FirstOrDefault();
+// Student student = context.Students.Where(x => x.Cgpa <= 3.2).Single();
+// Student student = context.Students.Where(x => x.Cgpa <= 3.2).SingleOrDefault();
+
+/*
+ApplicationDbContext context = new ApplicationDbContext();
+List<Student> students = context.Students.ToList();
+// List<Student> students = context.Students.Where(x => x.Cgpa <= 3.2 ).ToList();
+
+foreach (var student in students)
+{
+    Console.WriteLine($"Name : {student.Name}; Cgpa : {student.Cgpa}");
+}
+*/
+
+
+/* ---------- Update ---------- */
+
+/*
+ApplicationDbContext context = new ApplicationDbContext();
+List<Student> students = context.Students.Where(x => x.Cgpa <= 3.2 && x.DateOfBirth > new DateTime(2000, 12, 30)).ToList();
+
+foreach (var student in students)
+{
+    Console.WriteLine($"Name : {student.Name}; Cgpa : {student.Cgpa}");
+
+    student.Address = "Mirpur";
+}
+
+context.SaveChanges();
+*/
+
+
+/* ---------- Delete ---------- */
+
+/*
+ApplicationDbContext context = new ApplicationDbContext();
+Student s1 = context.Students.Where(x => x.Id == 2).FirstOrDefault();
+
+if(s1 != null)
+{
+    context.Students.Remove(s1);
+    context.SaveChanges();
+}
+*/
